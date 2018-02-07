3616724c1e73f9348df5fb1a0f60cfffbf6ee1d3
ezlanbe <benjamin.landry@ericsson.com>
2018-02-07 09:51:07 -0500

    can call to rbt in FT.py file

--- app/microservices/FT.py
+++ app/microservices/FT.py
@@ -17,43 +17,45 @@ rbt = api.namespace('RBT', description='Accessible by API')
 junit = api.namespace('JUnit', description='Accessible by API and CLI')
 rocket = api.namespace('Rocket', description='Accessible by API')
 
+host='142.133.174.149'
+port=':8888'
+#resource = '/AfgVafgMasterSmokeTestSuites.TestSuiteVafgAll.TestSuiteVafgEnafGba'
+#responder = '?suite'
+#http://142.133.174.149:8888/AfgVafgMasterSmokeTestSuites.TestSuiteVafgAll.TestSuiteVafgEnafGba?suite
 
+@rbt.route('/<string:resource>/<string:responder>/<string:tag>')  
+@rbt.param('resource', 'url project tracing, i.e. test_ID')
+@rbt.param('responder', 'editable file results')
+@rbt.param('tag', 'grouping parameter')
 
-@rbt.route('/<string:TC>/<string:responder>/<string:tag>/Test1')    
 class RBT(Resource):
 
-    @api.doc(responses={
+    @rbt.doc(responses={
         200: 'Success',
         400: 'Validation Error',
         500: 'Internal Server Error'
     })
 
-    def get(self, TC, responder, tag):
+    def get(self, resource, responder, tag):
         '''TC#1 Definition'''
+        url2 = 'http://' + host +  port + '/' + resource + '?' +responder
+        print(url2)
+
+
         url = 'http://httpbin.org/get'
+       # url = 'http://142.133.174.149:8888/AfgApTestCases.TestCase0800SuccessAfgApAutGbaDigest?test'
         tag = tag   # TODO: tag goes to a grouping file. Grouping would be done backend and hardcoded.
-        parameters = {"TC": TC, 'responder': responder}
-        r = requests.get(url, parameters)
+        parameters = {'resource': resource, 'responder':responder, 'tag': tag}
+        r = requests.get(url2)
         data = r.text
+      
         jsonData = json.loads(data)
 
-        with open('Tests_Logs.txt', 'a+') as outfile:
-            json.dump(jsonData, outfile, sort_keys = False, indent = 4,
-               ensure_ascii = False)
+        with open('Tests_Logs.txt', 'a') as outfile:
+           json.dump(jsonData, outfile, sort_keys = False, indent = 4,
+              ensure_ascii = False)
 
-        return jsonData
-
-    def get2(self):
-        url = 'http://httpbin.org/get'
-        r = requests.get(url)
-        data = r.text
-        jsonData = json.loads(data)
-        #specific_jsonData = json.loads(data)['headers']
-
-        with open('test_logs.txt', 'a+') as outfile: # in json-format
-            json.dump(jsonData, outfile, sort_keys = False, indent = 4, ensure_ascii = False)
-
-        return jsonData
+        return jsondata
 
 
 
